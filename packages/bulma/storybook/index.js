05c6d9dd06b12eb6619613d7d9b89f522234c393
Patrick Sachs <kontakt@patrick-sachs.de>
2018-09-06 22:15:37 +0200

    Add advanced form to bulma storybook

--- packages/bulma/storybook/index.js
+++ packages/bulma/storybook/index.js
@@ -2,12 +2,66 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Form from "@react-formilicious/bulma";
 import { action } from '@storybook/addon-actions';
-import options from './options';
+import range from "@react-formilicious/core/validators/range";
+import combined from "@react-formilicious/core/validators/combined";
+import required from "@react-formilicious/core/validators/required";
+import TextField from "@react-formilicious/bulma/TextField";
+import TextArea from "@react-formilicious/bulma/TextArea";
+import ToggleField from "@react-formilicious/bulma/ToggleField";
+import Checkbox from "@react-formilicious/bulma/Checkbox";
+import options, { fnNameOnly } from './options';
 
 storiesOf("Basic Form", module)
+
+  .addWithJSX("Advanced Form", () => (
+    <Form
+      elements={[
+        {
+          type: fnNameOnly(TextField),
+          key: "name",
+          name: "🙃 Username",
+          placeholder: "🙃 Enter your name here!",
+          // todo: we need a way to display the exact code below in the storybook.
+          validator: combined(
+            range({ min: 4, max: 16 })
+          )
+        },
+        {
+          type: fnNameOnly(TextField),
+          mode: "password",
+          key: "password",
+          name: "🔑 Password",
+          placeholder: "🔑 Your super secret password here!",
+          validator: range({ min: 6 })
+        },
+        {
+          type: fnNameOnly(TextArea),
+          key: "bio",
+          name: "🤖 Tell us something about you!",
+          placeholder: "🤖 What kind of developer robot are you?"
+        },
+        {
+          type: fnNameOnly(Checkbox),
+          key: "tos",
+          name: <span>📄 Accept the <a href="#" target="_self">TOS</a>?</span>,
+          validator: required()
+        },
+        {
+          type: fnNameOnly(ToggleField),
+          key: "newsletter",
+          name: null,
+          validator: required("You MUST subscribe to our totally-not-spam newsletter!"),
+          trueText: <span>📨 Subscribe to our <em>awesome</em> newsletter</span>,
+          falseText: "No newsletter 👎"
+        }
+      ]}
+      data={{}}
+      onSubmit={action("onSubmit")} />
+  ), options)
+
   .addWithJSX("Empty Form", () => (
     <Form elements={[]} data={{}} onSubmit={action("onSubmit")} />
-  ), options);
+  ), options)
 
 require("./Checkbox");
 require("./TagList");
--- packages/bulma/storybook/options.js
+++ packages/bulma/storybook/options.js
@@ -1,8 +1,50 @@
-const nameOnlySymbol = Symbol("name-only");
+const nameOnlySymbol = Symbol("react-formilicious/name-only");
+const fullBodySymbol = Symbol("react-formilicious/full-body");
+const literalSymbol = Symbol("react-formilicious/literal");
 
-export const nameOnly = fn => {
+export const fnNameOnly = fn => {
   fn[nameOnlySymbol] = true;
   return fn;
 };
 
-export default { showFunctions: false, functionValue: fn => fn[nameOnlySymbol] ? (fn.displayName || fn.name) : "() => { ... }" };
+export const fnFullBody = fn => {
+  fn[fullBodySymbol] = true;
+  return fn;
+};
+
+export const fnLiteralExpression = fn => {
+  const res = fn();
+  res[literalSymbol] = getFunctionBody(fn);
+  return res;
+};
+
+const getFunctionBody = fn => {
+  const str = fn.toString();
+  const matchOpen = str.match(/{\s*return/);
+    // We either have an arrow function with a return statement in it or a normal function.
+  if (matchOpen) {
+    const open = matchOpen.index + matchOpen[0].length + 1;
+    const matchClose = str.match(/;?\s*}(?=[^;?\s*}]*$)/);
+    const close = matchClose.index;
+    return str.slice(open, close).trim();
+  }
+  // It' s a one line arrow function
+  const arrowOpener = str.match(/(.*)\s*=>/);
+  const open = arrowOpener.index + arrowOpener[0].length + 1;
+  return str.substring(open).trim();
+}
+
+const functionValue = fn => {
+  if (fn[nameOnlySymbol]) {
+    return fn.displayName || fn.name;
+  }
+  if (fn[fullBodySymbol]) {
+    return fn.toString();
+  }
+  if (fn[literalSymbol]) {
+    return fn[literalSymbol].toString();
+  }
+  return "() => { ... }";
+}
+
+export default { showFunctions: false, functionValue };
